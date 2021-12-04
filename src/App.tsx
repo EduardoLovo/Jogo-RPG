2384848b89f7eac782aa9149e9062d25ed29c6d6
EduardoLovo <eduardo.lovo@gmail.com>
2021-12-04 19:13:24 -0300

    inserido component Character

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import * as C from './App.styles'
+import * as C from './App.styles';
+import { Character } from './components/character';
 
  const App = () => {
   return (
     <C.Container>
       <C.Map>
-        
+        <Character/>        
       </C.Map>
     </C.Container>
   )
